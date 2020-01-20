52122bf179365bd6f279be9b7705d706abcc4337
aSilas <silastj@hotmail.com>
2020-01-20 16:11:48 -0300

    Alterações nos icones do results

--- assets/js/green.js
+++ assets/js/green.js
@@ -1,17 +1,17 @@
 //FUNÇÃO SEND
-const send = document.querySelector("#send").addEventListener("click", function thankYou() {
-    // var email = document.getElementById("email").value;
-    // var name = document.getElementById("name").value;
-    // console.log(name)
-    // if (name == '' || email == '') {
-    //     alert("OI");
-    // } else {
-    const form = document.querySelector("#form")
-    form.classList.add("form-clean");
-    const formThank = document.querySelector("#form-thank")
-    formThank.classList.add("form-thank-ativo", "form-thank-anim");
-    send();
-});
+// const send = document.querySelector("#send").addEventListener("click", function thankYou() {
+//     // var email = document.getElementById("email").value;
+//     // var name = document.getElementById("name").value;
+//     // console.log(name)
+//     // if (name == '' || email == '') {
+//     //     alert("OI");
+//     // } else {
+//     const form = document.querySelector("#form")
+//     form.classList.add("form-clean");
+//     const formThank = document.querySelector("#form-thank")
+//     formThank.classList.add("form-thank-ativo", "form-thank-anim");
+//     send();
+// });
 
 
 
@@ -21,21 +21,24 @@ const send = document.querySelector("#send").addEventListener("click", function 
 
 //FORMULARIO
 const formulario = document.querySelector("#form-one");
-// console.log(formulario.elements.name.value);
-// console.log(document.forms[0]);
+
 function handleKeyUp(event) {
     const target = event.target;
     const email = document.querySelector("#email");
-    // console.log(email.value);
+    const labelEmail = document.querySelector(".email");
+
+    console.log(email.value);
 
     if (!target.checkValidity()) {
 
         target.classList.add('invalido');
-        target.setCustomValidity("! Please provide a valid e-mail.");
         target.nextElementSibling.innerText = target.validationMessage;
-        email.style.color = "#F71D1D";
+        target.setCustomValidity("! Please provide a valid e-mail.");
 
+        email.style.color = "#F71D1D";
+        labelEmail.style.color = "#F71D1D";
     }
+
     console.log(event.target.checkValidity());
     console.log(event.target.value);
 }
@@ -46,19 +49,23 @@ formulario.addEventListener('change', handleKeyUp);
 
 
 
-// function send() {
-//     var email = document.getElementById("email").value;
-//     var name = document.getElementById("name").value;
-//     // console.log(name, email);
-//     if (this.email == '' || this.name == '') {
-//         alert("Por Favor preencher os campos acima!!!");
-//     } else {
-//         // console.log(name);
-//         alert("Obrigado!!!");
+function send() {
+    const email = document.querySelector("#email").value;
+    const name = document.querySelector("#name_user").value;
+    const form = document.querySelector("#form")
 
-//     }
+    if (name == '' && email == '') {
+        alert("Por Favor preencher os campos acima!!!");
+    }
+    if (name != null && email == '') {
+        alert("por favor corrigir o email");
+    }
+    if (name == null && email == null) {
+        const formThank = document.querySelector("#form-thank")
+        form.classList.add("form-clean");
+        formThank.classList.add("form-thank-ativo", "form-thank-anim");
+    } else {
 
-// }
-// send();
+    }
 
-// alert("oi");
+}
