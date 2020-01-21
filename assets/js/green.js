a7a4aefe5c91a47cf0107cd499c283ac3d5d680b
aSilas <silastj@hotmail.com>
2020-01-20 21:17:13 -0300

    Versão 1.1

--- assets/js/green.js
+++ assets/js/green.js
@@ -1,94 +1,65 @@
-//FUNÇÃO SEND
-// const send = document.querySelector("#send").addEventListener("click", function thankYou() {
-//     // var email = document.getElementById("email").value;
-//     // var name = document.getElementById("name").value;
-//     // console.log(name)
-//     // if (name == '' || email == '') {
-//     //     alert("OI");
-//     // } else {
+// //FORMULARIO
+// const formulario = document.querySelector("#form-one");
+
+// function handleKeyUp(event) {
+//     var target = event.target;
+//     const email = document.querySelector("#email");
+//     const labelEmail = document.querySelector(".email");
+
+//     //console.log(email.value);
+
+//     if (!target.checkValidity()) {
+//         target.classList.add('invalido');
+//         target.nextElementSibling.innerText = target.validationMessage;
+//         target.setCustomValidity("! Please provide a valid e-mail.");
+//         email.style.color = "#F71D1D";
+//         labelEmail.style.color = "#F71D1D";
+//     } else {
+
+//         email.style.color = "#e6e6e6";
+//         labelEmail.style.color = "#e6e6e6";
+//     }
+
+//     console.log(event.target.checkValidity());
+//     console.log(event.target.value);
+// }
+
+// //formulario.addEventListener('change', handleKeyUp);
+
+// function send() {
+//     const email = document.querySelector("#email").value;
+//     const name = document.querySelector("#name_user").value;
 //     const form = document.querySelector("#form")
-//     form.classList.add("form-clean");
-//     const formThank = document.querySelector("#form-thank")
-//     formThank.classList.add("form-thank-ativo", "form-thank-anim");
-//     send();
-// });
 
+//     if (name == '' && email == '') {
 
+//         document.querySelector(".warning").classList.remove();
+//         document.querySelector(".warning").style.visibility = "visible";
+//         window.scroll({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//         setTimeout(function () {
+//             document.querySelector(".warning").style.visibility = "hidden";
+//         }, 3000)
+//     }
+//     if (name != null && email == '' || name == '' && email != null) {
 
+//         document.querySelector(".warning").classList.remove();
+//         document.querySelector(".warning").style.visibility = "visible";
+//         window.scroll({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//         setTimeout(function () {
+//             document.querySelector(".warning").style.visibility = "hidden";
+//         }, 3000)
+//     } else if (name !== null && email !== null) {
+//         //console.log(event.target.checkValidity);
+//         const formThank = document.querySelector("#form-thank");
 
+//         form.classList.add("form-clean");
+//         formThank.classList.add("form-thank-ativo", "form-thank-anim");
+//     }
 
-
-
-//FORMULARIO
-const formulario = document.querySelector("#form-one");
-
-function handleKeyUp(event) {
-    var target = event.target;
-    const email = document.querySelector("#email");
-    const labelEmail = document.querySelector(".email");
-
-    console.log(email.value);
-
-    if (!target.checkValidity()) {
-
-        target.classList.add('invalido');
-        target.nextElementSibling.innerText = target.validationMessage;
-        target.setCustomValidity("! Please provide a valid e-mail.");
-        email.style.color = "#F71D1D";
-        labelEmail.style.color = "#F71D1D";
-    }
-
-    console.log(event.target.checkValidity());
-    console.log(event.target.value);
-}
-formulario.addEventListener('change', handleKeyUp);
-
-
-
-
-
-
-function send() {
-    const email = document.querySelector("#email").value;
-    const name = document.querySelector("#name_user").value;
-    const form = document.querySelector("#form")
-    // var target = event.target;
-    // const aaa = target.checkValidity();
-    // const bbb = target.value;
-
-
-    if (name == '' && email == '') {
-
-        document.querySelector(".warning").classList.remove();
-        document.querySelector(".warning").style.visibility = "visible";
-        window.scroll({
-            top: 0,
-            behavior: "smooth"
-        });
-        setTimeout(function () {
-            document.querySelector(".warning").style.visibility = "hidden";
-        }, 3000)
-    }
-    if (name != null && email == '' || name == '' && email != null) {
-
-        document.querySelector(".warning").classList.remove();
-        document.querySelector(".warning").style.visibility = "visible";
-        window.scroll({
-            top: 0,
-            behavior: "smooth"
-        });
-        setTimeout(function () {
-            document.querySelector(".warning").style.visibility = "hidden";
-        }, 3000)
-    } else if (name !== null && email !== null) {
-        //console.log(event.target.checkValidity);
-        const formThank = document.querySelector("#form-thank");
-        window.scroll({
-            bottom: 0,
-            behavior: "smooth"
-        });
-        form.classList.add("form-clean");
-        formThank.classList.add("form-thank-ativo", "form-thank-anim");
-    }
-
-}
+// }
